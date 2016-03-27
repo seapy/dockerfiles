9a1ac2039deb1abb5c8e816bbdf3dd010f66863f
seapy <iamseapy@gmail.com>
2016-03-27 14:20:04 +0900

    remote log path from unicorn

--- rails-nginx-unicorn-pro/unicorn.rb
+++ rails-nginx-unicorn-pro/unicorn.rb
@@ -4,9 +4,6 @@ working_directory app_dir
  
 pid "#{app_dir}/tmp/unicorn.pid"
  
-stderr_path "#{app_dir}/log/unicorn.stderr.log"
-stdout_path "#{app_dir}/log/unicorn.stdout.log"
- 
 worker_processes 1
 listen "/tmp/unicorn.sock", :backlog => 64
 timeout 30
